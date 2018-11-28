bcb4b23292f2cd579f49c368c0aa7e8b556d2e0d
ocengineer <corbin.murrow@gmail.com>
2018-11-28 10:33:17 -0600

    Removal of obsolete views after consolidation

--- DNDToolkitApp/src/app/app.module.ts
+++ DNDToolkitApp/src/app/app.module.ts
@@ -3,21 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CombatantListComponent, EncounterListComponent, NewCombatantComponent, NewEncounterComponent } from './views';
 import { CombatantResolver, EncounterResolver } from './resolvers';
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CombatantComponent } from './views/combatant/combatant.component';
-import { EncounterComponent } from './views/encounter/encounter.component';
+import { CombatantComponent, EncounterComponent } from './views';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncounterListComponent,
-    NewEncounterComponent,
-    CombatantListComponent,
-    NewCombatantComponent,
     CombatantComponent,
     EncounterComponent
   ],
--- DNDToolkitApp/src/app/views/index.ts
+++ DNDToolkitApp/src/app/views/index.ts
@@ -1,6 +1,2 @@
-export { CombatantListComponent } from './combatant-list/combatant-list.component';
-export { EncounterListComponent } from './encounter-list/encounter-list.component';
-export { NewCombatantComponent } from './new-combatant/new-combatant.component';
-export { NewEncounterComponent } from './new-encounter/new-encounter.component';
 export { CombatantComponent } from './combatant/combatant.component';
 export { EncounterComponent } from './encounter/encounter.component';
